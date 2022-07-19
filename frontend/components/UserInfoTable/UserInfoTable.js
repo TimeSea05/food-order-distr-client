0c1e6b719b3c53d8aca5433aaaa14d2916dde961
Avalanche <anwentao1@gmail.com>
2022-07-19 14:35:19 +0800

    add personal info section

--- frontend/components/UserInfoTable/UserInfoTable.js
+++ frontend/components/UserInfoTable/UserInfoTable.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { UsernameContext } from '../../pages/_app'
 import DefaultUserAvatar from './DefaultUserAvatar'
+import { Divider } from 'rsuite'
 
 import UserInfoTableStyles from '../../styles/UserInfoTable.module.css'
 
 export default function UserInfotable () {
-  const { username } = React.useContext(UsernameContext)
+  const { userID, username, email } = React.useContext(UsernameContext)
   return (
-    <div className={UserInfoTableStyles['basic-info-wrapper']}>
-      <DefaultUserAvatar />
-      <span>Hello, {username}.</span>
+    <div className={UserInfoTableStyles['user-info-wrapper']}>
+      <div className={UserInfoTableStyles.greetings}>
+        <DefaultUserAvatar />
+        <span>Hello, {username}.</span>
+      </div>
+      <Divider />
+      <div className={UserInfoTableStyles['basic-info-wrapper']}>
+        <h3>Personal Info</h3>
+        <div className={UserInfoTableStyles['basic-info']}>
+          <ul>
+            <li>UserID: {userID}</li>
+            <li>Username: {username}</li>
+            <li>Email: {email}</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,8 +4,10 @@ import 'rsuite/dist/rsuite.min.css'
 import '../styles/globals.css'
 
 export const UsernameContext = React.createContext()
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
+  const [userID, setUserID] = React.useState('')
   const [username, setUsername] = React.useState('')
+  const [email, setEmail] = React.useState('')
 
   React.useEffect(() => {
     (
@@ -16,13 +18,17 @@ function MyApp({ Component, pageProps }) {
         })
 
         const content = await response.json()
-        if (content.name !== undefined) setUsername(content.name)
+        if (content.name !== undefined) {
+          setUserID(content.id)
+          setUsername(content.name)
+          setEmail(content.email)
+        }
       }
     )()
   })
 
   return (
-    <UsernameContext.Provider value={{username, setUsername}}>
+    <UsernameContext.Provider value={{ userID, username, setUsername, email }}>
       <Component {...pageProps} />
     </UsernameContext.Provider>
   )
